f6c783a61a680f71d672893eeeca5eac5e0685d1
kyle-park-io <andy3638@naver.com>
2024-07-27 18:44:06 +0900

    chore: add constant for api endpoint url

--- packages/backend/src/constants/constants.ts
+++ packages/backend/src/constants/constants.ts
@@ -1,3 +1,9 @@
+const isProd = process.env.NODE_ENV === 'prod' ? true : false;
+
 export const constants = {
-  apiPrefix: process.env.NODE_ENV === 'prod' ? 'api-dex' : '',
+  apiPrefix: isProd ? 'api-dex' : '',
+  api_url: isProd
+    ? 'https://jungho.dev/api-dex/api'
+    : 'http://localhost:3000/api',
+  prod_api_url: 'https://jungho.dev/api-dex/api',
 };
