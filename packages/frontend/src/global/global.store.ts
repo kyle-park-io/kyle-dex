85772d1fe70f86c248c1815a064a98c2522f8496
kyle-park-io <andy3638@naver.com>
2024-07-29 21:30:26 +0900

    feat: add global variable to track navigation

--- packages/frontend/src/global/global.store.ts
+++ packages/frontend/src/global/global.store.ts
@@ -1,6 +1,10 @@
 import { createStore } from 'solid-js/store';
 
 // global status
+export const [fromDexNavigate, setFromDexNavigate] = createStore({
+  value: false,
+});
+
 export const [globalNetwork, setGlobalNetwork] = createStore({
   network: 'hardhat',
 });
