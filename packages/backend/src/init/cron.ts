d064ff4e5b6d4276f9907def87c8603f88178df0
kyle-park-io <andy3638@naver.com>
2024-08-10 18:57:01 +0900

    chore: temporarily add API call to cron job

--- packages/backend/src/init/cron.ts
+++ packages/backend/src/init/cron.ts
@@ -11,12 +11,23 @@ export function startCronJobs() {
         await axios.get(`${constants.api_url}/network/getNetwork`, {
           params: { network: value },
         });
+        // TODO: network
+        await axios.get(`${constants.api_url}/network/reconnectNetwork`, {
+          params: { network: value },
+        });
+        await axios.get(`${constants.api_url}/network/reconnectEventListener`, {
+          params: { network: value },
+        });
       }
       console.log('Reconnect network success!', Date.now());
     } catch (err) {
       if (axios.isAxiosError(err) && err.config) {
         console.log('Try reconnect network: ', err.config.params.network);
         await axios.get(`${constants.api_url}/network/reconnectNetwork`, {
+          params: { network: err.config.params.network },
+        });
+        console.log('Try reconnect eventlistener: ', err.config.params.network);
+        await axios.get(`${constants.api_url}/network/reconnectEventListener`, {
           params: { network: err.config.params.network },
         });
       }
