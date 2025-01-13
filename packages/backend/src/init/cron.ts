915f7d020ccf533ec27f2c17ea4d585627c1c344
kyle-park-io <andy3638@naver.com>
2025-01-13 13:59:07 +0900

    fix: temporarily close event listener for bug resolution

--- packages/backend/src/init/cron.ts
+++ packages/backend/src/init/cron.ts
@@ -6,19 +6,19 @@ export function startCronJobs(): void {
   cron.schedule('*/5 * * * *', async () => {
     console.log('Check network status: ');
     try {
-      const network = ['hardhat', 'sepolia', 'amoy'];
-      for (const value of network) {
-        await axios.get(`${constants.api_url}/network/getNetwork`, {
-          params: { network: value },
-        });
-        // TODO: network
-        await axios.get(`${constants.api_url}/network/reconnectNetwork`, {
-          params: { network: value },
-        });
-        await axios.get(`${constants.api_url}/network/reconnectEventListener`, {
-          params: { network: value },
-        });
-      }
+      // const network = ['hardhat', 'sepolia', 'amoy'];
+      // for (const value of network) {
+      //   await axios.get(`${constants.api_url}/network/getNetwork`, {
+      //     params: { network: value },
+      //   });
+      //   // TODO: network
+      //   await axios.get(`${constants.api_url}/network/reconnectNetwork`, {
+      //     params: { network: value },
+      //   });
+      //   await axios.get(`${constants.api_url}/network/reconnectEventListener`, {
+      //     params: { network: value },
+      //   });
+      // }
       console.log('Reconnect network success!', Date.now());
     } catch (err) {
       if (axios.isAxiosError(err) && err.config != null) {
